30beb6cf28a5281d07a538de68edef11c378946f
satheeshKOLA532 <100255540+satheeshKOLA532@users.noreply.github.com>
2024-07-31 09:14:30 +0530

    main_kskill_eng.py

--- main_kskill_eng.py
+++ main_kskill_eng.py
@@ -28,7 +28,7 @@ def main():
     # </style>
     # """
     # st.markdown(html_css,unsafe_allow_html=True)
-    st.title("let's connect with Englih Brains...")
+    st.title("let's connect with English Brains...")
 
     # Initialize chat session in Streamlit if not already present
     if "chat_session" not in st.session_state:
