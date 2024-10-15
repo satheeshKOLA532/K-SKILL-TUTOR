674f5fb60ec4131f8ab2fc5520f7e839d5efac74
satheeshKOLA532 <100255540+satheeshKOLA532@users.noreply.github.com>
2024-10-15 15:18:32 +0530

    llm_med_coach.py

--- llm_med_coach.py
+++ llm_med_coach.py
@@ -114,7 +114,7 @@ def run_qa(message):
     print("message before sending to groq:",message)
     chat_completion = groq_client.chat.completions.create(
         messages=message,
-        model="llama-3.2-90b-vision-preview",
+        model="llama-3.1-8b-instant",
         max_tokens=2000,
         temperature=0.5,
         top_p=1,
